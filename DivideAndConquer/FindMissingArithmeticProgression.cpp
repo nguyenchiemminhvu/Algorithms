cfd28a9e28a8a4fb96abdcb1edf4e9e8a2240e1e
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2020-06-29 17:10:33 +0700

    update

---
+++ DivideAndConquer/FindMissingArithmeticProgression.cpp
@@ -0,0 +1,57 @@
+/*
+https://www.geeksforgeeks.org/find-missing-number-arithmetic-progression/
+
+Given an array that represents elements of arithmetic progression in order. One element is missing in the progression, find the missing number.
+Examples:
+
+Input: arr[]  = {2, 4, 8, 10, 12, 14}
+Output: 6
+
+Input: arr[]  = {1, 6, 11, 16, 21, 31};
+Output: 26
+
+We can solve this problem in O(Logn) time using Binary Search.
+The idea is to go to the middle element.
+Check if the difference between middle and next to middle is equal to diff or not,
+if not then the missing element lies between mid and mid+1.
+If the middle element is equal to n/2th term in Arithmetic Series
+(Let n be the number of elements in input array),
+then missing element lies in right half.
+Else element lies in left half.
+*/
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int FindMissingUtility(int * arr, int left, int right, int diff)
+{
+    if (left >= right)
+        return INT_MAX;
+    
+    return 0;
+}
+
+int FindMissing(int * arr, int n)
+{
+    int diff = (arr[n - 1] - arr[0]) / n;
+    return FindMissingUtility(arr, 0, n - 1, diff);
+}
+
+int main()
+{
+    int arr[] = {2, 4, 8, 10, 12, 14}; 
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    int missing = FindMissing(arr, n);
+    if (missing == INT_MAX)
+    {
+        std::cout << "Missing arithmetic progression can not be found" << std::endl;
+    }
+    else
+    {
+        std::cout << missing << std::endl;
+    }
+
+    return 0;
+}

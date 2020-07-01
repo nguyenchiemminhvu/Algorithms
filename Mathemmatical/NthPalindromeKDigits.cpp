97e583e7ad380c183b982f1d1508099de3f2fd77
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2020-07-01 16:28:56 +0700

    update

--- Mathemmatical/NthPalindromeKDigits.cpp
+++ Mathemmatical/NthPalindromeKDigits.cpp
@@ -17,18 +17,47 @@ Output : 103301
 */
 
 #include <iostream>
-#include <string>
+#include <cmath>
 using namespace std;
 
-std::string NthPalindromeKDigits(std::string n)
+int CountDigit(int n)
 {
+    int res = 0;
+    while (n)
+    {
+        n /= 10;
+        res++;
+    }
+    return res;
+}
 
+int NthPalindromeKDigits(int n, int k)
+{
+    int temp = (k & 1) ? (k / 2) : (k/2 - 1); 
+    int firstHalf = (int)pow(10, temp); 
+    firstHalf += n - 1;
+  
+    temp = firstHalf;
+    // If k is odd, truncate the last digit 
+    if (k & 1) 
+        temp /= 10;
+    
+    int numDigit = CountDigit(temp);
+
+    int secondHalf = 0;
+    while (temp)
+    { 
+        secondHalf = secondHalf * 10 + temp % 10;
+        temp /= 10;
+    }
+    
+    return firstHalf * (int)pow(10, numDigit) + secondHalf;
 }
 
 int main()
 {
-    std::string n = "534976";
-    std::cout << NthPalindromeKDigits(n) << std::endl;
+    int n = 6, k = 6;
+    std::cout << NthPalindromeKDigits(n, k) << std::endl;
 
     return 0;
 }

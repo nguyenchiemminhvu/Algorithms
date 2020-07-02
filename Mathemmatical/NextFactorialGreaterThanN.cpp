751d813ae1d95cf1e2dc5a2fe14fc4a26e1b22dc
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2020-07-02 10:12:28 +0700

    update

---
+++ Mathemmatical/NextFactorialGreaterThanN.cpp
@@ -0,0 +1,31 @@
+/*
+https://www.geeksforgeeks.org/find-the-next-factorial-greater-than-n/
+
+Given a number N (≤ 1018), the task is to find the next factorial number greater than N.
+
+Examples:
+
+Input: N = 24
+Output: 120
+Explanation:
+As 4! = 24. So the next number which factorial and greater than 24 is 5!, which is 120
+
+Input: N = 150
+Output: 720
+Explanation:
+As 5! = 120. So the next number which factorial and greater than 150 is 6!, which is 720.
+*/
+
+#include <iostream>
+using namespace std;
+
+int NextFactorialGreaterThan(int n)
+{
+    
+}
+
+int main()
+{
+    std::cout << NextFactorialGreaterThan(100) << std::endl;
+    return 0;
+}

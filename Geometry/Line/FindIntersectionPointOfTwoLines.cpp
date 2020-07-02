160e9b20c45124da66684aa8fb3e1327b835fd25
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2020-07-02 17:13:25 +0700

    update

---
+++ Geometry/Line/FindIntersectionPointOfTwoLines.cpp
@@ -0,0 +1,36 @@
+/*
+https://www.geeksforgeeks.org/program-for-point-of-intersection-of-two-lines/
+
+Given points A and B corresponding to line AB and points P and Q corresponding to line PQ, find the point of intersection of these lines. The points are given in 2D Plane with their X and Y Coordinates.
+
+Examples:
+
+Input : A = (1, 1), B = (4, 4)
+        C = (1, 8), D = (2, 4)
+Output : The intersection of the given lines 
+         AB and CD is: (2.4, 2.4)
+
+Input : A = (0, 1), B = (0, 4)
+        C = (1, 8), D = (1, 4)
+Output : The given lines AB and CD are parallel.
+*/
+
+#include <iostream>
+using namespace std;
+
+struct Point
+{
+    int _x;
+    int _y;
+
+    Point(int x = 0, int y = 0)
+        : _x(x), _y(y)
+    {
+
+    }
+};
+
+int main()
+{
+    return 0;
+}

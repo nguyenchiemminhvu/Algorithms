be6fdef5dffc39fe349948c7b44838605080e38f
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2020-07-02 11:35:33 +0700

    update

--- Mathemmatical/FactorialOfLargeNumber.cpp
+++ Mathemmatical/FactorialOfLargeNumber.cpp
@@ -15,6 +15,23 @@ Output : 933262154439441526816992388562667004-
 Input :50
 Output : 3041409320171337804361260816606476884-
          4377641568960512000000000000
+
+factorial(n)
+1) Create an array ‘res[]’ of MAX size where MAX is number of maximum digits in output.
+2) Initialize value stored in ‘res[]’ as 1 and initialize ‘res_size’ (size of ‘res[]’) as 1.
+3) Do following for all numbers from x = 2 to n.
+……a) Multiply x with res[] and update res[] and res_size to store the multiplication result.
+
+How to multiply a number ‘x’ with the number stored in res[]?
+The idea is to use simple school mathematics. We one by one multiply x with every digit of res[]. The important point to note here is digits are multiplied from rightmost digit to leftmost digit. If we store digits in same order in res[], then it becomes difficult to update res[] without extra space. That is why res[] is maintained in reverse way, i.e., digits from right to left are stored.
+
+multiply(res[], x)
+1) Initialize carry as 0.
+2) Do following for i = 0 to res_size – 1
+….a) Find value of res[i] * x + carry. Let this value be prod.
+….b) Update res[i] by storing last digit of prod in it.
+….c) Update carry by storing remaining digits in carry.
+3) Put all digits of carry in res[] and increase res_size by number of digits in carry.
 */
 
 #include <iostream>

96ee0cedd2cac16b8700bdfb7b8676667156ce60
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2020-08-10 13:19:07 +0700

    update

---
+++ Bits/FindNthNumberHasPalindromeBits.cpp
@@ -0,0 +1,33 @@
+/*
+https://www.geeksforgeeks.org/find-n-th-number-whose-binary-representation-palindrome/
+
+Find the nth number whose binary representation is a palindrome. Do not consider the leading zeros, while considering the binary representation. Consider the 1st number whose binary representation is palindrome as 1, instead of 0
+
+Examples:
+
+Input : 1
+Output : 1
+1st Number whose binary representation 
+is palindrome is 1 (1)
+
+Input : 9
+Output : 27
+9th Number whose binary representation
+is palindrome is 27 (11011)
+*/
+
+#include <iostream>
+using namespace std;
+
+int KthPalindromeBits(int k)
+{
+    return 0;
+}
+
+int main()
+{
+    int k = 9;
+    std::cout << KthPalindromeBits(k) << std::endl;
+
+    return 0;
+}

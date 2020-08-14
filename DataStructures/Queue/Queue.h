980cd05c671fcbd958bdd6c2efb8dd19723782b1
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2020-08-15 01:30:42 +0700

    update

---
+++ DataStructures/Queue/Queue.h
@@ -0,0 +1,8 @@
+#ifndef __QUEUE_H__
+#define __QUEUE_H__
+
+#include "LinkedList.h"
+
+
+
+#endif // __QUEUE_H__
---
+++ DataStructures/Stack/Stack.h
@@ -0,0 +1,8 @@
+#ifndef __STACK_H__
+#define __STACK_H__
+
+#include "LinkedList.h"
+
+
+
+#endif // __STACK_H__

31bb06ff965b98297415faed650b266d2fa8d09d
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2020-08-15 12:06:25 +0700

    update

--- DataStructures/Queue/Queue.h
+++ DataStructures/Queue/Queue.h
@@ -3,6 +3,38 @@
 
 #include "LinkedList.h"
 
+template <typename T>
+class Queue
+{
+public:
 
+    Queue()
+    {
+    }
+
+    ~Queue()
+    {
+        L.Clear();
+    }
+
+    bool Push(const T &v)
+    {
+        return true;
+    }
+
+    void Pop()
+    {
+
+    }
+
+    T Front()
+    {
+        
+    }
+
+private:
+
+    LinkedList<T> L;
+};
 
 #endif // __QUEUE_H__
--- DataStructures/Stack/Stack.h
+++ DataStructures/Stack/Stack.h
@@ -3,6 +3,39 @@
 
 #include "LinkedList.h"
 
+template <typename T>
+class Stack
+{
+public:
+
+    Stack()
+    {
+    }
+
+    ~Stack()
+    {
+        L.Clear();
+    }
+
+    bool Push(const T &v)
+    {
+        return true;
+    }
+
+    void Pop()
+    {
+
+    }
+
+    T Top()
+    {
+        
+    }
+
+private:
+
+    LinkedList<T> L;
+};
 
 
 #endif // __STACK_H__

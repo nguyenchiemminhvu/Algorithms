cc2d55ec2cee6309eaae1f8b38aa26cb6e4cefd9
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2020-09-08 15:48:53 +0700

    update

--- DataStructures/Tree/Tree.h
+++ DataStructures/Tree/Tree.h
@@ -49,8 +49,8 @@ class BSTree
 {
 public:
     BSTree()
+        : root(NULL)
     {
-
     }
 
     virtual ~BSTree()
@@ -60,12 +60,33 @@ public:
 
     virtual bool Insert(T value)
     {
-        return false;
+        TreeNode<T> * node = CreateNode(value);
+        if (!node)
+            return false;
+        
+        root = InsertUtil(root, node);
+        return true;
     }
 
     TreeNode<T> * Find(T value)
     {
-        return NULL;
+        if (!root)
+            return NULL;
+        
+        TreeNode<T> * p = root;
+        while (p && p->data != value)
+        {
+            if (p->data > value)
+            {
+                p = p->left;
+            }
+            else 
+            {
+                p = p->right;
+            }
+        }
+
+        return p;
     }
 
     virtual bool Delete(T value)
@@ -75,22 +96,22 @@ public:
 
     void PrintPreOrder()
     {
-
+        PrintPreOrderUtil(root);
     }
 
     void PrintInOrder()
     {
-
+        PrintInOrderUtil(root);
     }
 
     void PrintPostOrder()
     {
-
+        PrintPostOrderUtil(root);
     }
 
     void PrintLevelOrder()
     {
-
+        PrintLevelOrderUtil(root);
     }
 
     int Size()
@@ -103,23 +124,154 @@ public:
         return 0;
     }
 
+    bool IsEmpty()
+    {
+        return root == NULL;
+    }
+
     void Clear()
     {
+        std::queue<TreeNode<T> *> Q;
+        Q.push(root);
 
+        while (!Q.empty())
+        {
+            TreeNode<T> * node = Q.front();
+            Q.pop();
+
+            if (node->left)
+            {
+                Q.push(node->left);
+            }
+            if (node->right)
+            {
+                Q.push(node->right);
+            }
+
+            delete node;
+        }
     }
 
     TreeNode<T> * Min()
     {
-        return NULL;
+        TreeNode<T> * p = root;
+        while (p && p->left)
+        {
+            p = p->left;
+        }
+
+        return p;
     }
 
     TreeNode<T> * Max()
     {
-        return NULL;
+        TreeNode<T> * p = root;
+        while (p && p->right)
+        {
+            p = p->right;
+        }
+
+        return p;
     }
 
 protected:
 
+    TreeNode<T> * CreateNode(T v)
+    {
+        TreeNode<T> * node = new TreeNode<T>(v);
+        return node;
+    }
+
+    virtual TreeNode<T> * InsertUtil(TreeNode<T> * root, TreeNode<T> * node)
+    {
+        if (!root)
+        {
+            return node;
+        }
+
+        if (root->data == node->data)
+        {
+            return root;
+        }
+
+        if (root->data > node->data)
+        {
+            root->left = InsertUtil(root->left, node);
+        }
+        else if (root->data < node->data)
+        {
+            root->right = InsertUtil(root->right, node);
+        }
+
+        return root;
+    }
+
+    void PrintPreOrderUtil(TreeNode<T> * root)
+    {
+        if (!root)
+            return;
+        
+        std::cout << root->data << " ";
+        PrintPreOrderUtil(root->left);
+        PrintPreOrderUtil(root->right);
+    }
+
+    void PrintInOrderUtil(TreeNode<T> * root)
+    {
+        if (!root)
+            return;
+
+        PrintInOrderUtil(root->left);
+        std::cout << root->data << " ";
+        PrintInOrderUtil(root->right);
+    }
+
+    void PrintPostOrderUtil(TreeNode<T> * root)
+    {
+        if (!root)
+            return;
+
+        PrintPostOrderUtil(root->left);
+        PrintPostOrderUtil(root->right);
+        std::cout << root->data << " ";
+    }
+
+    void PrintLevelOrderUtil(TreeNode<T> * root)
+    {
+        std::queue<TreeNode<T> *> Q;
+        int curLevel = 1, nextLevel = 0;
+        Q.push(root);
+
+        while (!Q.empty())
+        {
+            TreeNode<T> * node = Q.front();
+            Q.pop();
+            curLevel--;
+
+            std::cout << node->data << " ";
+
+            if (node->left)
+            {
+                Q.push(node->left);
+                nextLevel++;
+            }
+            if (node->right)
+            {
+                Q.push(node->right);
+                nextLevel++;
+            }
+
+            if (curLevel == 0)
+            {
+                curLevel = nextLevel;
+                nextLevel = 0;
+
+                std::cout << std::endl;
+            }
+        }
+    }
+
+protected:
     TreeNode<T> * root;
 };
 

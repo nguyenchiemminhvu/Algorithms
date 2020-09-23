f379d2b36ccee002d4282543a0d2e0792024b4c3
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2020-09-23 16:15:50 +0700

    update

---
+++ DataStructures/Trie/Trie.cpp
@@ -0,0 +1,28 @@
+#include "Trie.h"
+
+int main()
+{
+    Trie T;
+    T.Insert("cash");
+    T.Insert("card");
+    T.Insert("last");
+    T.Insert("part");
+    T.Insert("count");
+    T.Insert("program");
+    T.Insert("application");
+
+    if (T.Search("program"))
+    {
+        std::cout << "Found: program" << std::endl;
+    }
+
+    T.Print();
+    std::cout << std::endl;
+
+    T.Delete("count");
+
+    T.Print();
+    std::cout << std::endl;
+
+    return 0;
+}
--- DataStructures/Trie/Trie.h
+++ DataStructures/Trie/Trie.h
@@ -1,14 +1,22 @@
 #ifndef __TRIE_H__
 #define __TRIE_H__
 
+#include <vector>
+#include <set>
+#include <stack>
+#include <queue>
+#include <limits.h>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
 #define ALPHABET_SIZE 26
 
-template <typename T>
 class TrieNode
 {
 public:
     TrieNode()
-        : isWord(false)
+        : isLeaf(false)
     {
 
     }
@@ -20,8 +28,96 @@ public:
 
 public:
 
-    TrieNode * children[ALPHABET_SIZE];
-    bool isWord;
+    TrieNode * child_nodes[ALPHABET_SIZE];
+    bool isLeaf;
+};
+
+class Trie
+{
+public:
+    Trie()
+    {
+        root = CreateNode();
+    }
+
+    ~Trie()
+    {
+
+    }
+
+    bool IsEmpty()
+    {
+        return root == NULL;
+    }
+
+    void Insert(std::string key)
+    {
+        root = InsertUtil(root, key);
+    }
+
+    bool Search(std::string key)
+    {
+        return SearchUtil(root, key);
+    }
+
+    void Delete(std::string key)
+    {
+        root = DeleteUtil(root, key);
+    }
+
+    void Clear()
+    {
+        root = ClearUtil(root);
+    }
+
+    void Print()
+    {
+        PrintUtil(root);
+    }
+
+protected:
+
+    TrieNode * CreateNode()
+    {
+        TrieNode * node = new TrieNode();
+        
+        node->isLeaf = false;
+        for (int i = 0; i < ALPHABET_SIZE; i++)
+        {
+            node->child_nodes[i] = NULL;
+        }
+
+        return node;
+    }
+
+    TrieNode * InsertUtil(TrieNode * root, std::string key)
+    {
+        return NULL;
+    }
+
+    bool SearchUtil(TrieNode * root, std::string key)
+    {
+        return false;
+    }
+
+    TrieNode * DeleteUtil(TrieNode * root, std::string key)
+    {
+        return NULL;
+    }
+
+    TrieNode * ClearUtil(TrieNode * root)
+    {
+        return NULL;
+    }
+
+    void PrintUtil(TrieNode * root)
+    {
+        
+    }
+
+protected:
+
+    TrieNode * root;
 };
 
 #endif // __TRIE_H__

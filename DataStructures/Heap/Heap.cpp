44b769ff9e81d5b2ea137f23cfa63f5183c63e8d
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2020-09-29 15:07:53 +0700

    update

---
+++ DataStructures/Heap/Heap.cpp
@@ -0,0 +1,9 @@
+#include "Heap.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
---
+++ DataStructures/Heap/Heap.h
@@ -0,0 +1,23 @@
+#ifndef __HEAP_H__
+#define __HEAP_H__
+
+template <typename T>
+class Heap
+{
+public:
+    Heap()
+    {
+
+    }
+
+    ~Heap()
+    {
+
+    }
+
+protected:
+
+    
+};
+
+#endif // __HEAP_H__

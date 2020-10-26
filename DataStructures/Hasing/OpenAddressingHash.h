37d5216f90d0ba558e074eaf159d1463dae99d97
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2020-10-26 13:46:31 +0700

    update

--- DataStructures/Hasing/OpenAddressingHash.h
+++ DataStructures/Hasing/OpenAddressingHash.h
@@ -1,13 +1,17 @@
 #ifndef __OPEN_ADDRESSING_HASH_H__
 #define __OPEN_ADDRESSING_HASH_H__
 
+#include <vector>
+#include <iostream>
+
 template <typename T>
 class OAHash
 {
 public:
-    OAHash()
+    OAHash(int b)
+        : BUCKET(b)
     {
-
+        table.resize(b);
     }
 
     ~OAHash()
@@ -15,8 +19,35 @@ public:
 
     }
 
+    int Hash(T value)
+    {
+        return 0;
+    }
+
+    void Insert(T value)
+    {
+
+    }
+
+    bool Search(T value)
+    {
+        return false;
+    }
+
+    void Delete(T value)
+    {
+
+    }
+
+    void Print()
+    {
+
+    }
+
 private:
-    
+
+    int BUCKET;
+    typename std::vector<std::vector<T>> table;
 };
 
 #endif // __OPEN_ADDRESSING_HASH_H__

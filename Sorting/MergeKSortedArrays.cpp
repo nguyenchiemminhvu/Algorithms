83627e8d8a5f1f43e7bfa267613d88536fc0388c
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2021-01-19 10:32:15 +0700

    update

--- Sorting/MergeKSortedArrays.cpp
+++ Sorting/MergeKSortedArrays.cpp
@@ -23,10 +23,47 @@ Explanation: The output array is a sorted array that contains all the elements o
 */
 
 #include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
+
+void MergeKSortedArrays(const std::vector<std::vector<int>> &kArrays, std::vector<int> &out)
+{
+    using PIP = std::pair<int, std::pair<int, int>>;
+    std::priority_queue<PIP, std::vector<PIP>, std::greater<PIP>> PQ;
+    for (int i = 0; i < kArrays.size(); i++)
+    {
+        PQ.push( { kArrays[i][0], { i, 0 } } );
+    }
+
+    int io = 0;
+    while (!PQ.empty())
+    {
+        PIP cur = PQ.top();
+        PQ.pop();
+
+        out[io++] = cur.first;
+
+        int i = cur.second.first;
+        int j = cur.second.second;
+        if (j + 1 < kArrays[i].size())
+        {
+            PQ.push( { kArrays[i][j + 1], { i, j + 1 } } );
+        }
+    }
+}
 
 int main()
 {
-    
+    std::vector<std::vector<int>> kArrays = {{2, 6, 12},
+                                            {1, 9, 20, 1000},
+                                            {23, 34, 90, 2000, 3000}};
+
+    std::vector<int> merged(kArrays.size() * kArrays[0].size());
+    MergeKSortedArrays(kArrays, merged);
+    for (int n : merged)
+        std::cout << n << " ";
+    std::cout << std::endl;
+
     return 0;
 }

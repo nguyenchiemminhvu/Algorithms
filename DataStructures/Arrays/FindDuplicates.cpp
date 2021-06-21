502dd16b8339e6fc69274493ffae77dcaae9c190
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2021-06-21 15:51:37 +0700

    update

--- DataStructures/Arrays/FindDuplicates.cpp
+++ DataStructures/Arrays/FindDuplicates.cpp
@@ -27,20 +27,18 @@ void FindDuplicates(int * arr, int n)
 {
     for (int i = 0; i < n; i++)
     {
-        if (arr[abs(arr[i])] >= 0)
-        {
-            arr[abs(arr[i])] = -arr[abs(arr[i])];
-        }
-        else
+        if (arr[abs(arr[i])] < 0)
         {
             std::cout << abs(arr[i]) << std::endl;
         }
+        
+        arr[abs(arr[i])] = -arr[abs(arr[i])];
     }   
 }
 
 int main()
 {
-    int arr[] = { 1, 2, 3, 1, 3, 6, 6 };
+    int arr[] = { 1, 2, 3, 1, 3, 6, 6, 6 };
     int n = sizeof(arr) / sizeof(arr[0]);
 
     FindDuplicates(arr, n);
